146398a7e8d43a69953639d574ff50e4ac236316
Clément Pannetier <clement.pannetier49@gmail.com>
2019-03-26 13:42:25 +0100

    refactor js / css import and bootstrap theme added

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,18 +2,31 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
+    // Custom
     gulp.src('assets/scss/style.scss')
     .pipe(sass())
+    .pipe(gulp.dest('public/static/css/'));
+    // DataTables Boostrap
+    gulp.src('node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css')
     .pipe(gulp.dest('public/static/css/'));
 });
 
 gulp.task('js', function() {
+    // JQuery
     gulp.src('node_modules/jquery/dist/jquery.min.js')
     .pipe(gulp.dest('public/static/js/'));
+    // Bootstrap
     gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
     .pipe(gulp.dest('public/static/js/'));
+    // Popper
     gulp.src('node_modules/popper.js/dist/umd/popper.min.js')
     .pipe(gulp.dest('public/static/js/'));
+    // DataTables
+    gulp.src('node_modules/datatables.net/js/jquery.dataTables.min.js')
+    .pipe(gulp.dest('public/static/js/'));
+    gulp.src('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js')
+    .pipe(gulp.dest('public/static/js/'));
+    // Custom
     gulp.src('assets/js/scripts.js')
     .pipe(gulp.dest('public/static/js/'));
 });
