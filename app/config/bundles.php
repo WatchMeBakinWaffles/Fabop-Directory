b6064a23f21a21d38685d7a2c0566ffd8249fcf8
Matthieu Bernard <matthieu4699@gmail.com>
2020-03-11 14:38:53 +0100

    intégration de svelte

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -17,4 +17,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
