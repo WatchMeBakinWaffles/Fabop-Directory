9f862dbbd75c002a49890173e81379035d247577
Matt <mbe1>
2020-03-20 11:25:22 +0100

    liste user api composant

---
+++ app/assets/js/tableau.svelte
@@ -0,0 +1,27 @@
+<h2><i class="fas fa-users"></i> Liste des personnes</h2>
+<div class="row m-3">
+<a class="btn btn-primary mr-2" href="/manager/people/new"><i class="fas fa-plus"></i> Ajouter</a>
+<span class="not-allowed"><a class="btn text-warning mr-2" href="/manager/imp-exp/"><i class="fas fa-file-import"></i> Import des données</a></span>
+<span class="not-allowed"><a class="btn text-warning mr-2" href="/manager/imp-exp/" id="exportClick"><i class="fas fa-file-export"></i> Export des données</a></span>
+</div>
+<table class="table table-striped dataTable" id="DataTables_Table_0">
+	<thead class="bg-secondary text-white">
+		<tr>
+			<th><input type="checkbox" id="select-all"></th>
+			<th>Nom</th>
+			<th>Prénom</th>
+			<th>Date de naissance</th>
+			<th>Abonnement à la newsletter</th>
+			<th>Code postal</th>
+			<th>Ville</th>
+			<th>Date d'ajout</th>
+			<th><i class="fas fa-cog"></i></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td colspan="10">Aucune entrée</td>
+		</tr>
+	</tbody>
+	
+</table> 
