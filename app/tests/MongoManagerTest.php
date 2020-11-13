60751faf0a0ebdbe00552874bb3823de24d5ea59
AlexDamas <adamasceno@DESKTOP-UJSUNS0.localdomain>
2020-11-13 11:18:05 +0100

    mongodb test

--- app/tests/MongoManagerTest.php
+++ app/tests/MongoManagerTest.php
@@ -1,15 +1,17 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-use App\Utils\MongoManager;
+namespace App\Tests;
+
 use App\Exception\DocumentNotFoundException;
+use App\Utils\MongoManager;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Validator\Tests\Fixtures\ToString;
 
 class MongoManagerTest extends TestCase
 {
 
     public function test_Constructor_none_noException(){
-        $manager=new MongoManager();
+        $manager = new MongoManager();
         $this->addToAssertionCount(1);
         // $manager->debug();
     }
@@ -17,15 +19,15 @@ class MongoManagerTest extends TestCase
     public function test_RetrieveDocumentByID_NotExistingDocument_Exception(){
         $this->expectException(DocumentNotFoundException::class);
         $manager=new MongoManager();
-        $doc=$manager->getDocById('Entity_person_sheet','5bf015a4b96e4020ec3f3984');
+        $doc=$manager->getDocById('Entity_person_sheet','5fa41615c7827056697a1ea3');
         // $manager->debug();
     }
 
     public function test_RetrieveDocumentByID_ExistingDocumentById_JSONStringOfTheBSONObject(){
         $manager=new MongoManager();
         $this->assertEquals(
-            '{ "_id" : { "$oid" : "5bf015a4b96e4020ec3f3983" }, "name" : "John" }',
-            $manager->getDocById("Entity_person_sheet","5bf015a4b96e4020ec3f3983"));
+            '{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" }, "name" : "John" }',
+            $manager->getDocById("Entity_person_sheet","5fa41615c7827056697a1ea3"));
         // $manager->var_dump();
         // $manager->debug();
     }
@@ -33,8 +35,8 @@ class MongoManagerTest extends TestCase
     public function test_RetrieveDocumentByIDWithProjection_ExistingDocumentById_JSONStringOfTheBSONObject(){
         $manager=new MongoManager();
         $this->assertEquals(
-            '{ "_id" : { "$oid" : "5bf015a4b96e4020ec3f3983" } }',
-            $manager->getDocById("Entity_person_sheet","5bf015a4b96e4020ec3f3983",['_id'=>1]));
+            '{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" } }',
+            $manager->getDocById("Entity_person_sheet","5fa41615c7827056697a1ea3",['_id'=>1]));
         // $manager->var_dump();
         // $manager->debug();
     }
@@ -42,7 +44,7 @@ class MongoManagerTest extends TestCase
     public function test_RetrieveDocumentByFilter_ExistingDocumentByMail_JSONStringOfTheBSONObject(){
         $manager=new MongoManager();
         $this->assertEquals(
-            ['{ "_id" : { "$oid" : "5c18e4529d01a864bce4be3a" }, "name" : "machin", "emails" : [ "1@gmail.com", "2@gmail.com", "3@gmail.com", "sample@mail.com" ] }'],
+            ['{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" }, "name" : "machin", "emails" : [ "1@gmail.com", "2@gmail.com", "3@gmail.com", "sample@mail.com" ] }'],
             $manager->getDocByFilter("Entity_person_sheet",["emails"=>["1@gmail.com"]]));
         // $manager->debug();
     }
@@ -50,7 +52,7 @@ class MongoManagerTest extends TestCase
     public function test_RetrieveDocumentByFilterWithProjection_ExistingDocumentByMail_JSONStringOfTheBSONObject(){
         $manager=new MongoManager();
         $this->assertEquals(
-            ['{ "_id" : { "$oid" : "5c18e4529d01a864bce4be3a" }, "emails" : [ "1@gmail.com", "2@gmail.com", "3@gmail.com", "sample@mail.com" ] }'],
+            ['{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" }, "emails" : [ "1@gmail.com", "2@gmail.com", "3@gmail.com", "sample@mail.com" ] }'],
             $manager->getDocByFilter("Entity_person_sheet",["emails"=>["1@gmail.com"]],['emails'=>1]));
         // $manager->debug();
     }
@@ -58,7 +60,7 @@ class MongoManagerTest extends TestCase
     public function test_RetrieveDocumentByFilter_ExistingDocumentByPhone_JSONStringOfTheBSONObject(){
         $manager=new MongoManager();
         $this->assertEquals(
-            ['{ "_id" : { "$oid" : "5c18eddd9d01a864bce4be3e" }, "name" : "truc", "phones" : [ "0698995173", "0605487915", "0739425487" ] }'],
+            ['{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" }, "name" : "truc", "phones" : [ "0698995173", "0605487915", "0739425487" ] }'],
             $manager->getDocByFilter("Entity_person_sheet",["phones"=>["0739425487"]]));
         // $manager->debug();
     }
@@ -66,8 +68,8 @@ class MongoManagerTest extends TestCase
     public function test_RetrieveDocumentByFilter_ExistingMultipleDocumentsByMail_JSONStringOfTheBSONObject(){
         $manager=new MongoManager();
         $this->assertEquals(
-            ['{ "_id" : { "$oid" : "5c18e4529d01a864bce4be3a" }, "name" : "machin", "emails" : [ "1@gmail.com", "2@gmail.com", "3@gmail.com", "sample@mail.com" ] }',
-            '{ "_id" : { "$oid" : "5c1baeb79d01a83c4c54a1e4" }, "name" : "sample", "emails" : "sample@mail.com" }'],
+            ['{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" }, "name" : "machin", "emails" : [ "1@gmail.com", "2@gmail.com", "3@gmail.com", "sample@mail.com" ] }',
+            '{ "_id" : { "$oid" : "5fa41615c7827056697a1ea3" }, "name" : "sample", "emails" : "sample@mail.com" }'],
             $manager->getDocByFilter("Entity_person_sheet",["emails"=>["sample@mail.com"]]));
         // $manager->debug();
     }
@@ -88,38 +90,38 @@ class MongoManagerTest extends TestCase
     public function test_DeleteDocument_ExistentDocument_DocumentDeleted(){
         $manager= new MongoManager();
         $this->assertEquals(1,
-            $manager->deleteSingleById("Entity_person_sheet","5c1bd8936e7dcf0149085eb2")
+            $manager->deleteSingleById("Entity_person_sheet","5fa41615c7827056697a1ea3")
         );
     }
 
     public function test_DeleteDocument_InexistentDocument_Exception(){
         $this->expectException(DocumentNotFoundException::class);
         $manager= new MongoManager();
-        $manager->deleteSingleById("Entity_person_sheet","5c1bd8936e7dcf0149085eb7");
+        $manager->deleteSingleById("Entity_person_sheet","5fa41615c7827056697a1ea3");
     }
 
     public function test_UpdateDocument_ExistentDocument_DocumentUpdated(){
         $manager= new MongoManager();
         $this->assertEquals(1,
-            $manager->updateSingleValueById("Entity_person_sheet","5c1bd8936e7dcf0149085eb2","emails.0","sample3@mail.com")
+            $manager->updateSingleValueById("Entity_person_sheet","5fa41615c7827056697a1ea3","emails.0","sample3@mail.com")
         );
     }
 
     public function test_UpdateDocument_InexistentDocument_Exception(){
         $this->expectException(DocumentNotFoundException::class);
         $manager= new MongoManager();
-        $manager->updateSingleValueById("Entity_person_sheet","5c1bd8936e7dcf0149085eb9","emails.0","sample3@mail.com");
+        $manager->updateSingleValueById("Entity_person_sheet","5fa41615c7827056697a1ea3","emails.0","sample3@mail.com");
     }
 
     public function test_UnsetSingleValue_ExistentDocument_Exception(){
         $manager= new MongoManager();
-        $manager->unsetSingleValueById("Entity_person_sheet","5c337c725464de34c86873e6","name");
+        $manager->unsetSingleValueById("Entity_person_sheet","5fa41615c7827056697a1ea3","name");
         $this->assertTrue(true);
     }
 
     public function test_UnsetSingleValue_InexistentDocument_Exception(){
         $manager= new MongoManager();
-        $manager->unsetSingleValueById("Entity_person_sheet","5c337c725464de34c86873e0","name");
+        $manager->unsetSingleValueById("Entity_person_sheet","5fa41615c7827056697a1ea3","name");
         $this->assertTrue(true);
     }
 
