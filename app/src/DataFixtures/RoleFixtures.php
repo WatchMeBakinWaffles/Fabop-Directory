fa36c45ad5d784d0d1f59ef0d9216f6ced35dc3d
Carneiro <erwan2000c@gmail.com>
2020-11-17 18:21:19 +0100

    fixtures liaison user avec role et deplacement des insertions

--- app/src/DataFixtures/RoleFixtures.php
+++ app/src/DataFixtures/RoleFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\EntityUser;
 use App\Entity\EntityRoles;
 use App\Entity\Permissions;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -83,6 +84,17 @@ class RoleFixtures extends Fixture
         $manager->persist($entityRoleAdmin);
         $manager->persist($entityRoleContributeur);
         $manager->persist($entityRoleUtilisateur);
+
+	$user_root = new EntityUser();
+        $user_root->setEmail("root@root.fr");
+        $user_root->setRoles(array("ROLE_ADMIN"));
+	$user_root->addEntityRole($entityRoleAdmin);
+        $user_root->setFirstname("root");
+        $user_root->setLastname("root");
+        $user_root->setPassword('$2y$12$eGhtebnhdb0BDs72VBLYneNt6Fz9QYM5FLL6P86irhhwahamUDhEm');
+        $user_root->setApiToken("API_ROOT");
+
+        $manager->persist($user_root);
 
         $manager->flush();
     }
