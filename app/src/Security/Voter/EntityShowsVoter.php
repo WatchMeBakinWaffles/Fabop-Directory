d2a406fbfc2800cc30c69f07d189382e9122a45f
Carneiro <erwan2000c@gmail.com>
2020-11-18 19:55:36 +0100

    ajout du voter pour show et controler

--- app/src/Security/Voter/EntityShowsVoter.php
+++ app/src/Security/Voter/EntityShowsVoter.php
@@ -2,21 +2,18 @@
 
 namespace App\Security\Voter;
 
-use App\Entity\EntityRoles;
-use App\Entity\Permissions;
-use App\Utils\MongoManager;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class EntityInstitutionsVoter extends Voter
+class EntityShowsVoter extends Voter
 {
     protected function supports($attribute, $subject)
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
         return in_array($attribute, ['POST_EDIT', 'POST_VIEW'])
-            && $subject instanceof \App\Entity\EntityInstitutions;
+            && $subject instanceof \App\Entity\EntityShows;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
@@ -26,7 +23,7 @@ class EntityInstitutionsVoter extends Voter
         if (!$user instanceof UserInterface) {
             return false;
         }
-
+	
 	$roles = $user->getEntityRoles();
 	foreach ($roles as $role){
 		$permissions = $role->getPermissions();
@@ -35,11 +32,15 @@ class EntityInstitutionsVoter extends Voter
 		// ... (check conditions and return true to grant permission) ...
 		switch ($attribute) {
 		    case 'POST_EDIT':
-		        if ($data_permissions["institutions"] = "W" || $data_permissions["institutions"] = "RW"){
+		        // logic to determine if the user can EDIT
+		        // return true or false
+		        if ($data_permissions["shows"] = "W" || $data_permissions["shows"] = "RW"){
 		         	return true;
 			}
 		    case 'POST_VIEW':
-		        if ($data_permissions["institutions"] = "R" || $data_permissions["institutions"] = "RW"){
+		        // logic to determine if the user can VIEW
+		        // return true or false
+		        if ($data_permissions["shows"] = "R" || $data_permissions["shows"] = "RW"){
 		         	return true;
 			}
 		}
