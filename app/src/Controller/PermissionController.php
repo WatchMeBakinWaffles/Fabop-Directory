0cb5e810d19e83ff3a16447e1f4a6d34f2836558
Dorian Pelletier <pelletierdorian@gmail.com>
2021-03-22 17:52:04 +0100

    formulaire permissions 22-03

--- app/src/Controller/PermissionController.php
+++ app/src/Controller/PermissionController.php
@@ -2,13 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\EntityInstitutions;
-use App\Entity\EntityModele;
-use App\Entity\EntityPeople;
-use App\Entity\EntityPerformances;
-use App\Entity\EntityRoles;
-use App\Entity\EntityShows;
-use App\Entity\EntityTags;
 use App\Entity\EntityUser;
 use App\Repository\EntityRolesRepository;
 use App\Repository\PermissionsRepository;
@@ -22,7 +15,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,42 +28,35 @@ class PermissionController extends AbstractController
      */
     public function index(Request $request): Response
     {
-
         $choiceEntity = array(
-            'Institution'=> EntityInstitutions::class,
-            'Modèle'=> EntityModele::class,
-            'Personne' => EntityPeople::class,
-            'Rôle' => EntityRoles::class,
-            'Spectacle' => EntityShows::class,
-            'Tag' => EntityTags::class,
-            'Utilisateur'=> EntityUser::class,
+            'Chat' => 'cat',
+            'Chien' => 'dog',
+            'Tortue'=> 'turtle',
+            'Poisson' => 'fish'
         );
-
         $useFilter = array(
             'Oui' => 'oui',
             'Non' => 'non',
         );
 
-        $em = $this->getDoctrine()->getManager();
-
         $form = $this->createFormBuilder()
             ->add('nom_de_la_permission', TextType::class, array('required' => true))
             ->add('entite',ChoiceType::class, ['choices' => $choiceEntity])
             ->add('ajouter_un_filtre',ChoiceType::class, array(
-            'choices' => $useFilter,
-            'expanded' => false,
+                //'dataclass' => 'useFilter',
+                'choices' => $useFilter,
+                'expanded' => false,
             ))
             ->getForm();
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
             if (isset($data['ajouter_un_filtre']) && $data['ajouter_un_filtre'] === 'oui') {
-                return $this->redirectToRoute('permission_filter', array('data' => $data));
+                return $this->redirectToRoute('permission_filter', $data);
             }
             else if (isset($data['ajouter_un_filtre']) && $data['ajouter_un_filtre'] === 'non') {
-                return $this->redirectToRoute('permission_create', array('data' => $data));
+                return $this->render('permission/permission_created.html.twig', $data);
             }
         }
         return $this->render('permission/index.html.twig', [
@@ -86,23 +71,22 @@ class PermissionController extends AbstractController
      */
     public function permission_filter(Request $request): Response
     {
-        $data = $request->get('data');
-
         $choiceRights = array(
             'Oui' => 'oui',
             'Non' => 'non',
             'Inchangés' => 'inchanges',
         );
-
-        $em = $this->getDoctrine()->getManager();
+        $choiceField = array(
+            'Email' => 'email',
+            'Role' => 'role',
+        );
 
         $form = $this->createFormBuilder()
-            ->add('champ_a_filtrer', ChoiceType::class, array(
-                'choices' => $em->getClassMetadata($data['entite']) ->getColumnNames(),
-                'choice_label' => function ($value){
-                    return $value;
-                },))
-            ->add('valeur_du_filtre', TextType::class, array('required' => true))
+            ->add('champ_a_filtrer',ChoiceType::class, [
+            'choices' => $choiceField,
+            'expanded'=>false,
+            ])
+            ->add('valeur_du_filtre', null, array('required' => true))
             ->add('droits_lecture',ChoiceType::class, [
             'choices' => $choiceRights,
             'expanded'=>false,
@@ -111,46 +95,13 @@ class PermissionController extends AbstractController
             'choices' => $choiceRights,
             'expanded'=>false,
             ])
-        ->getForm();
 
+        ->getForm();
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = array_merge($data, $form->getData());
-            return $this->redirectToRoute('permission_create', array('data' => $data));
-        }
-
-        return $this->render('permission/permission_filter.html.twig', ['form' => $form->createView(), 'data' => $data]);
-
+        return $this->render('permission/permission_filter.html.twig', [
+            'form' => $form->createView(),
+        ]);
             }
 
-
-    /**
-     * @Route("/permission_create", name="permission_create")
-     * @param Request $request
-     * @return Response
-     */
-    public function permission_create(Request $request): Response
-    {
-        $data = $request->get('data');
-        $jsonData = new JsonResponse($data);
-
-        $traduction = array(
-            EntityShows::class => "show",
-            EntityTags::class => "tags",
-            EntityPeople::class => "peoples",
-            EntityUser::class => "users",
-            EntityModele::class => "models",
-            EntityInstitutions::class => "institutions",
-            EntityRoles::class => "roles"
-        );
-
-        var_dump($jsonData);
-
-        return $this->render('permission/permission_create.html.twig', ['data' => $data]);
-
-    }
-
-
 }
-
